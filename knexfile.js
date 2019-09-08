f95fd3dfcdc93721c3ac73bc0aac2236b15c4fd6
ericlugo <ericlugo@users.noreply.github.com>
2019-09-08 02:10:48 -0400

    Boilerplating done. Updated README again...

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/users.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+  testing: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/testing.db3',
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'dbmigrations',
+    },
+    seeds: {
+      directory: './data/seeds',
+    },
+  },
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// const authRouter = require('./routers/auth-router.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(morgan('dev'));
+server.use(express.json());
+
+// server.use('/', authRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+    <h1>Looking for API Information?</h1>
+    <p>Please see the <a href='https://github.com/buildweek-devlibs/backend-auth'>README</a> for more information on where to go!</p>
+  `);
+});
+
+module.exports = server;
